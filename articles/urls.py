852ec6d48bbdaad4eaab837721feedb149d12f27
Ritabrata Das <dasritabrata5@gmail.com>
2021-05-06 02:26:41 +0530

    Add files via upload

---
+++ articles/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.views import logout
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+     path('', views.index, name='index'),
+    path('articles/', views.chat_view, name='chats'),
+    path('articles/<int:sender>/<int:receiver>/', views.message_view, name='chat'),
+    path('api/messages/<int:sender>/<int:receiver>/', views.message_list, name='message-detail'),
+    path('api/messages/', views.message_list, name='message-list'),
+    path('logout/', logout, {'next_page': 'index'}, name='logout'),
+    path('register/', views.register_view, name='register'),
+]
+
